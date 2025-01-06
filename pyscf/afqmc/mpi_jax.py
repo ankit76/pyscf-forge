08ae2d6f91133ef81d972487d029ba7d7b36cbc3
Ankit Mahajan <ankitmahajan76@gmail.com>
2025-01-06 13:04:03 -0500

    copied over files from ad_afqmc

---
+++ pyscf/afqmc/mpi_jax.py
@@ -0,0 +1,245 @@
+import argparse
+import pickle
+import time
+
+import config
+import h5py
+import numpy as np
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--use_gpu", action="store_true")
+    args = parser.parse_args()
+
+    if args.use_gpu:
+        config.afqmc_config["use_gpu"] = True
+
+config.setup_jax()
+MPI = config.setup_comm()
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+from functools import partial
+
+import driver
+import hamiltonian
+import propagation
+import sampling
+import wavefunctions
+from jax import numpy as jnp
+
+print = partial(print, flush=True)
+
+
+def _prep_afqmc(options=None):
+    if rank == 0:
+        print(f"# Number of MPI ranks: {size}\n#")
+
+    with h5py.File("FCIDUMP_chol", "r") as fh5:
+        [nelec, nmo, ms, nchol] = fh5["header"]
+        h0 = jnp.array(fh5.get("energy_core"))
+        h1 = jnp.array(fh5.get("hcore")).reshape(nmo, nmo)
+        chol = jnp.array(fh5.get("chol")).reshape(-1, nmo, nmo)
+
+    assert type(ms) is np.int64
+    assert type(nelec) is np.int64
+    assert type(nmo) is np.int64
+    assert type(nchol) is np.int64
+    ms, nelec, nmo, nchol = int(ms), int(nelec), int(nmo), int(nchol)
+    nelec_sp = ((nelec + abs(ms)) // 2, (nelec - abs(ms)) // 2)
+
+    norb = nmo
+
+    if options is None:
+        try:
+            with open("options.bin", "rb") as f:
+                options = pickle.load(f)
+        except:
+            options = {}
+
+    options["dt"] = options.get("dt", 0.01)
+    options["n_walkers"] = options.get("n_walkers", 50)
+    options["n_prop_steps"] = options.get("n_prop_steps", 50)
+    options["n_ene_blocks"] = options.get("n_ene_blocks", 50)
+    options["n_sr_blocks"] = options.get("n_sr_blocks", 1)
+    options["n_blocks"] = options.get("n_blocks", 50)
+    options["n_ene_blocks_eql"] = options.get("n_ene_blocks_eql", 5)
+    options["n_sr_blocks_eql"] = options.get("n_sr_blocks_eql", 10)
+    options["seed"] = options.get("seed", np.random.randint(1, int(1e6)))
+    options["n_eql"] = options.get("n_eql", 1)
+    options["ad_mode"] = options.get("ad_mode", None)
+    assert options["ad_mode"] in [None, "forward", "reverse", "2rdm"]
+    options["orbital_rotation"] = options.get("orbital_rotation", True)
+    options["do_sr"] = options.get("do_sr", True)
+    options["walker_type"] = options.get("walker_type", "rhf")
+    options["symmetry"] = options.get("symmetry", False)
+    options["save_walkers"] = options.get("save_walkers", False)
+    options["trial"] = options.get("trial", None)
+    if options["trial"] is None:
+        if rank == 0:
+            print(f"# No trial specified in options.")
+    options["ene0"] = options.get("ene0", 0.0)
+    options["free_projection"] = options.get("free_projection", False)
+    options["n_batch"] = options.get("n_batch", 1)
+
+    try:
+        with h5py.File("observable.h5", "r") as fh5:
+            [observable_constant] = fh5["constant"]
+            observable_op = np.array(fh5.get("op")).reshape(nmo, nmo)
+            if options["walker_type"] == "uhf":
+                observable_op = jnp.array([observable_op, observable_op])
+            observable = [observable_op, observable_constant]
+    except:
+        observable = None
+
+    ham = hamiltonian.hamiltonian(nmo)
+    ham_data = {}
+    ham_data["h0"] = h0
+    ham_data["h1"] = jnp.array([h1, h1])
+    ham_data["chol"] = chol.reshape(nchol, -1)
+    ham_data["ene0"] = options["ene0"]
+
+    wave_data = {}
+    mo_coeff = jnp.array(np.load("mo_coeff.npz")["mo_coeff"])
+    wave_data["rdm1"] = jnp.array(
+        [
+            mo_coeff[0][:, : nelec_sp[0]] @ mo_coeff[0][:, : nelec_sp[0]].T,
+            mo_coeff[1][:, : nelec_sp[1]] @ mo_coeff[1][:, : nelec_sp[1]].T,
+        ]
+    )
+
+    if options["trial"] == "rhf":
+        trial = wavefunctions.rhf(norb, nelec_sp, n_batch=options["n_batch"])
+        wave_data["mo_coeff"] = mo_coeff[0][:, : nelec_sp[0]]
+    elif options["trial"] == "uhf":
+        trial = wavefunctions.uhf(norb, nelec_sp, n_batch=options["n_batch"])
+        wave_data["mo_coeff"] = [
+            mo_coeff[0][:, : nelec_sp[0]],
+            mo_coeff[1][:, : nelec_sp[1]],
+        ]
+    elif options["trial"] == "noci":
+        with open("dets.pkl", "rb") as f:
+            ci_coeffs_dets = pickle.load(f)
+        ci_coeffs_dets = [
+            jnp.array(ci_coeffs_dets[0]),
+            [jnp.array(ci_coeffs_dets[1][0]), jnp.array(ci_coeffs_dets[1][1])],
+        ]
+        wave_data["ci_coeffs_dets"] = ci_coeffs_dets
+        trial = wavefunctions.noci(
+            norb, nelec_sp, ci_coeffs_dets[0].size, n_batch=options["n_batch"]
+        )
+    elif options["trial"] == "cisd":
+        try:
+            amplitudes = np.load("amplitudes.npz")
+            ci1 = jnp.array(amplitudes["ci1"])
+            ci2 = jnp.array(amplitudes["ci2"])
+            trial_wave_data = {"ci1": ci1, "ci2": ci2}
+            wave_data.update(trial_wave_data)
+            trial = wavefunctions.cisd(norb, nelec_sp, n_batch=options["n_batch"])
+        except:
+            raise ValueError("Trial specified as cisd, but amplitudes.npz not found.")
+    elif options["trial"] == "ucisd":
+        try:
+            amplitudes = np.load("amplitudes.npz")
+            ci1a = jnp.array(amplitudes["ci1a"])
+            ci1b = jnp.array(amplitudes["ci1b"])
+            ci2aa = jnp.array(amplitudes["ci2aa"])
+            ci2ab = jnp.array(amplitudes["ci2ab"])
+            ci2bb = jnp.array(amplitudes["ci2bb"])
+            trial_wave_data = {
+                "ci1A": ci1a,
+                "ci1B": ci1b,
+                "ci2AA": ci2aa,
+                "ci2AB": ci2ab,
+                "ci2BB": ci2bb,
+                "mo_coeff": mo_coeff,
+            }
+            wave_data.update(trial_wave_data)
+            trial = wavefunctions.ucisd(norb, nelec_sp, n_batch=options["n_batch"])
+        except:
+            raise ValueError("Trial specified as ucisd, but amplitudes.npz not found.")
+    else:
+        try:
+            with open("trial.pkl", "rb") as f:
+                [trial, trial_wave_data] = pickle.load(f)
+            wave_data.update(trial_wave_data)
+            if rank == 0:
+                print(f"# Read trial of type {type(trial).__name__} from trial.pkl.")
+        except:
+            if rank == 0:
+                print(
+                    "# trial.pkl not found, make sure to construct the trial separately."
+                )
+            trial = None
+
+    if options["walker_type"] == "rhf":
+        if options["symmetry"]:
+            ham_data["mask"] = jnp.where(jnp.abs(ham_data["h1"]) > 1.0e-10, 1.0, 0.0)
+        else:
+            ham_data["mask"] = jnp.ones(ham_data["h1"].shape)
+
+        prop = propagation.propagator_restricted(
+            options["dt"], options["n_walkers"], n_batch=options["n_batch"]
+        )
+
+    elif options["walker_type"] == "uhf":
+        if options["symmetry"]:
+            ham_data["mask"] = jnp.where(jnp.abs(ham_data["h1"]) > 1.0e-10, 1.0, 0.0)
+        else:
+            ham_data["mask"] = jnp.ones(ham_data["h1"].shape)
+        if options["free_projection"]:
+            prop = propagation.propagator_unrestricted(
+                options["dt"],
+                options["n_walkers"],
+                10,
+                n_batch=options["n_batch"],
+            )
+        else:
+            prop = propagation.propagator_unrestricted(
+                options["dt"],
+                options["n_walkers"],
+                n_batch=options["n_batch"],
+            )
+
+    sampler = sampling.sampler(
+        options["n_prop_steps"],
+        options["n_ene_blocks"],
+        options["n_sr_blocks"],
+        options["n_blocks"],
+    )
+
+    if rank == 0:
+        print(f"# norb: {norb}")
+        print(f"# nelec: {nelec_sp}")
+        print("#")
+        for op in options:
+            if options[op] is not None:
+                print(f"# {op}: {options[op]}")
+        print("#")
+
+    return ham_data, ham, prop, trial, wave_data, sampler, observable, options, MPI
+
+
+if __name__ == "__main__":
+    ham_data, ham, prop, trial, wave_data, sampler, observable, options, _ = (
+        _prep_afqmc()
+    )
+    assert trial is not None
+    init = time.time()
+    comm.Barrier()
+    e_afqmc, err_afqmc = 0.0, 0.0
+    if options["free_projection"]:
+        driver.fp_afqmc(
+            ham_data, ham, prop, trial, wave_data, sampler, observable, options, MPI
+        )
+    else:
+        e_afqmc, err_afqmc = driver.afqmc(
+            ham_data, ham, prop, trial, wave_data, sampler, observable, options, MPI
+        )
+    comm.Barrier()
+    end = time.time()
+    if rank == 0:
+        print(f"ph_afqmc walltime: {end - init}", flush=True)
+        np.savetxt("ene_err.txt", np.array([e_afqmc, err_afqmc]))
+    comm.Barrier()
---
+++ pyscf/afqmc/sampling.py
@@ -0,0 +1,362 @@
+from dataclasses import dataclass
+from functools import partial
+from typing import Any, Tuple
+
+import jax
+import jax.numpy as jnp
+import linalg_utils
+from hamiltonian import hamiltonian
+from jax import checkpoint, jit, lax, random
+from propagation import propagator
+from wavefunctions import wave_function
+
+
+@dataclass
+class sampler:
+    n_prop_steps: int = 50
+    n_ene_blocks: int = 50
+    n_sr_blocks: int = 1
+    n_blocks: int = 50
+
+    @partial(jit, static_argnums=(0, 4, 5))
+    def _step_scan(
+        self,
+        prop_data: dict,
+        fields: jax.Array,
+        ham_data: dict,
+        prop: propagator,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[dict, jax.Array]:
+        """Phaseless propagation scan function over steps."""
+        prop_data = prop.propagate(trial, ham_data, prop_data, fields, wave_data)
+        return prop_data, fields
+
+    @partial(jit, static_argnums=(0, 4, 5))
+    def _step_scan_free(
+        self,
+        prop_data: dict,
+        fields: jax.Array,
+        ham_data: dict,
+        prop: propagator,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[dict, jax.Array]:
+        """Free propagation scan function over steps."""
+        prop_data = prop.propagate_free(trial, ham_data, prop_data, fields, wave_data)
+        return prop_data, fields
+
+    @partial(jit, static_argnums=(0, 4, 5))
+    def _block_scan(
+        self,
+        prop_data: dict,
+        _x: Any,
+        ham_data: dict,
+        prop: propagator,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[dict, Tuple[jax.Array, jax.Array]]:
+        """Block scan function. Propagation and energy calculation."""
+        prop_data["key"], subkey = random.split(prop_data["key"])
+        fields = random.normal(
+            subkey,
+            shape=(
+                self.n_prop_steps,
+                prop.n_walkers,
+                ham_data["chol"].shape[0],
+            ),
+        )
+        _step_scan_wrapper = lambda x, y: self._step_scan(
+            x, y, ham_data, prop, trial, wave_data
+        )
+        prop_data, _ = lax.scan(_step_scan_wrapper, prop_data, fields)
+        prop_data["n_killed_walkers"] += prop_data["weights"].size - jnp.count_nonzero(
+            prop_data["weights"]
+        )
+        prop_data = prop.orthonormalize_walkers(prop_data)
+        prop_data["overlaps"] = trial.calc_overlap(prop_data["walkers"], wave_data)
+        energy_samples = jnp.real(
+            trial.calc_energy(prop_data["walkers"], ham_data, wave_data)
+        )
+        energy_samples = jnp.where(
+            jnp.abs(energy_samples - prop_data["e_estimate"]) > jnp.sqrt(2.0 / prop.dt),
+            prop_data["e_estimate"],
+            energy_samples,
+        )
+        block_weight = jnp.sum(prop_data["weights"])
+        block_energy = jnp.sum(energy_samples * prop_data["weights"]) / block_weight
+        prop_data["pop_control_ene_shift"] = (
+            0.9 * prop_data["pop_control_ene_shift"] + 0.1 * block_energy
+        )
+        return prop_data, (block_energy, block_weight)
+
+    @partial(jit, static_argnums=(0, 4, 5))
+    def _block_scan_free(
+        self,
+        prop_data: dict,
+        _x: Any,
+        ham_data: dict,
+        prop: propagator,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[dict, Tuple[dict, jax.Array, jax.Array]]:
+        """Block scan function for free propagation."""
+        prop_data["key"], subkey = random.split(prop_data["key"])
+        fields = random.normal(
+            subkey,
+            shape=(
+                self.n_prop_steps,
+                prop.n_walkers,
+                ham_data["chol"].shape[0],
+            ),
+        )
+        _step_scan_wrapper = lambda x, y: self._step_scan_free(
+            x, y, ham_data, prop, trial, wave_data
+        )
+        prop_data, _ = lax.scan(_step_scan_wrapper, prop_data, fields)
+        energy_samples = trial.calc_energy(prop_data["walkers"], ham_data, wave_data)
+        # energy_samples = jnp.where(jnp.abs(energy_samples - ham_data['ene0']) > jnp.sqrt(2./propagator.dt), ham_data['ene0'],     energy_samples)
+        block_energy = jnp.sum(energy_samples * prop_data["overlaps"]) / jnp.sum(
+            prop_data["overlaps"]
+        )
+        block_weight = jnp.sum(prop_data["overlaps"])
+        return prop_data, (prop_data, block_energy, block_weight)
+
+    @partial(jit, static_argnums=(0, 4, 5))
+    def _sr_block_scan(
+        self,
+        prop_data: dict,
+        _x: Any,
+        ham_data: dict,
+        prop: propagator,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[dict, Tuple[jax.Array, jax.Array]]:
+        _block_scan_wrapper = lambda x, y: self._block_scan(
+            x, y, ham_data, prop, trial, wave_data
+        )
+        prop_data, (block_energy, block_weight) = lax.scan(
+            _block_scan_wrapper, prop_data, None, length=self.n_ene_blocks
+        )
+        prop_data = prop.stochastic_reconfiguration_local(prop_data)
+        prop_data["overlaps"] = trial.calc_overlap(prop_data["walkers"], wave_data)
+        return prop_data, (block_energy, block_weight)
+
+    @partial(jit, static_argnums=(0, 3, 4))
+    def _ad_block(
+        self,
+        prop_data: dict,
+        ham_data: dict,
+        prop: propagator,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[dict, Tuple[jax.Array, jax.Array]]:
+        _sr_block_scan_wrapper = lambda x, y: self._sr_block_scan(
+            x, y, ham_data, prop, trial, wave_data
+        )
+
+        prop_data["overlaps"] = trial.calc_overlap(prop_data["walkers"], wave_data)
+        prop_data["n_killed_walkers"] = 0
+        prop_data["pop_control_ene_shift"] = prop_data["e_estimate"]
+        prop_data, (block_energy, block_weight) = lax.scan(
+            checkpoint(_sr_block_scan_wrapper),
+            prop_data,
+            None,
+            length=self.n_sr_blocks,
+        )
+        prop_data["n_killed_walkers"] /= (
+            self.n_sr_blocks * self.n_ene_blocks * prop.n_walkers
+        )
+        return prop_data, (block_energy, block_weight)
+
+    @partial(jit, static_argnums=(0, 1, 5, 7))
+    def propagate_phaseless_ad(
+        self,
+        ham: hamiltonian,
+        ham_data: dict,
+        coupling: float,
+        observable_op: jax.Array,
+        prop: propagator,
+        prop_data: dict,
+        trial: wave_function,
+        wave_data: Any,
+    ) -> Tuple[jax.Array, dict]:
+        ham_data["h1"] = ham_data["h1"] + coupling * observable_op
+        wave_data = trial.optimize(ham_data, wave_data)
+        # ham_data = ham.rot_orbs(ham_data, wave_data)
+        ham_data = ham.build_measurement_intermediates(ham_data, trial, wave_data)
+        ham_data = ham.build_propagation_intermediates(ham_data, prop, trial, wave_data)
+
+        prop_data, (block_energy, block_weight) = self._ad_block(
+            prop_data, ham_data, prop, trial, wave_data
+        )
+        return jnp.sum(block_energy * block_weight) / jnp.sum(block_weight), prop_data
+
+    # for 2-rdm
+    @partial(jit, static_argnums=(0, 1, 5, 7))
+    def propagate_phaseless_ad_1(
+        self,
+        ham: hamiltonian,
+        ham_data: dict,
+        coupling: float,
+        observable_op: jax.Array,
+        prop: propagator,
+        prop_data: dict,
+        trial: wave_function,
+        wave_data: dict,
+    ):
+        # modify ham_data
+        observable_op = (
+            observable_op
+            + jnp.transpose(observable_op, (2, 3, 0, 1))
+            + jnp.transpose(observable_op, (1, 0, 3, 2))
+            + jnp.transpose(observable_op, (3, 2, 1, 0))
+        ) / 4.0
+        norb = ham.norb
+        ham_data["chol"] = linalg_utils.modified_cholesky(
+            observable_op.reshape(norb**2, norb**2), norb, ham_data["chol"].shape[0]
+        )
+        wave_data = trial.optimize(ham_data, wave_data)
+        # ham_data = ham.rot_orbs(ham_data, wave_data)
+        ham_data = ham.build_measurement_intermediates(ham_data, trial, wave_data)
+        ham_data = ham.build_propagation_intermediates(ham_data, prop, trial, wave_data)
+
+        prop_data, (block_energy, block_weight) = self._ad_block(
+            prop_data, ham_data, prop, trial, wave_data
+        )
+        return jnp.sum(block_energy * block_weight) / jnp.sum(block_weight), prop_data
+
+    @partial(jit, static_argnums=(0, 1, 5, 7))
+    def propagate_phaseless_ad_nosr(
+        self,
+        ham: hamiltonian,
+        ham_data: dict,
+        coupling: float,
+        observable_op: jax.Array,
+        prop: propagator,
+        prop_data: dict,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[jax.Array, dict]:
+        ham_data["h1"] = ham_data["h1"] + coupling * observable_op
+        wave_data = trial.optimize(ham_data, wave_data)
+        # ham_data = ham.rot_orbs(ham_data, wave_data)
+        ham_data = ham.build_measurement_intermediates(ham_data, trial, wave_data)
+        ham_data = ham.build_propagation_intermediates(ham_data, prop, trial, wave_data)
+
+        prop_data["overlaps"] = trial.calc_overlap(prop_data["walkers"], wave_data)
+        prop_data["n_killed_walkers"] = 0
+        prop_data["pop_control_ene_shift"] = prop_data["e_estimate"]
+        _block_scan_wrapper = lambda x, y: self._block_scan(
+            x, y, ham_data, prop, trial, wave_data
+        )
+        prop_data, (block_energy, block_weight) = lax.scan(
+            checkpoint(_block_scan_wrapper),
+            prop_data,
+            None,
+            length=self.n_ene_blocks,
+        )
+        prop_data["n_killed_walkers"] /= (
+            self.n_sr_blocks * self.n_ene_blocks * prop.n_walkers
+        )
+        return jnp.sum(block_energy * block_weight) / jnp.sum(block_weight), prop_data
+
+    @partial(jit, static_argnums=(0, 1, 5, 7))
+    def propagate_phaseless_ad_norot(
+        self,
+        ham: hamiltonian,
+        ham_data: dict,
+        coupling: float,
+        observable_op: jax.Array,
+        prop: propagator,
+        prop_data: dict,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[jax.Array, dict]:
+        ham_data["h1"] = ham_data["h1"] + coupling * observable_op
+        ham_data = ham.build_measurement_intermediates(ham_data, trial, wave_data)
+        ham_data = ham.build_propagation_intermediates(ham_data, prop, trial, wave_data)
+
+        prop_data, (block_energy, block_weight) = self._ad_block(
+            prop_data, ham_data, prop, trial, wave_data
+        )
+        return jnp.sum(block_energy * block_weight) / jnp.sum(block_weight), prop_data
+
+    @partial(jit, static_argnums=(0, 1, 5, 7))
+    def propagate_phaseless_ad_nosr_norot(
+        self,
+        ham: hamiltonian,
+        ham_data: dict,
+        coupling: float,
+        observable_op: jax.Array,
+        prop: propagator,
+        prop_data: dict,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[jax.Array, dict]:
+        ham_data["h1"] = ham_data["h1"] + coupling * observable_op
+        ham_data = ham.build_measurement_intermediates(ham_data, wave_data)
+        ham_data = ham.build_propagation_intermediates(ham_data, prop, trial, wave_data)
+
+        def _block_scan_wrapper(x, y):
+            return self._block_scan(x, y, ham_data, prop, trial, wave_data)
+
+        prop_data["overlaps"] = trial.calc_overlap(prop_data["walkers"], wave_data)
+        prop_data["n_killed_walkers"] = 0
+        prop_data["pop_control_ene_shift"] = prop_data["e_estimate"]
+        prop_data, (block_energy, block_weight) = lax.scan(
+            checkpoint(_block_scan_wrapper),
+            prop_data,
+            None,
+            length=self.n_ene_blocks,
+        )
+        prop_data["n_killed_walkers"] /= (
+            self.n_sr_blocks * self.n_ene_blocks * prop.n_walkers
+        )
+        return jnp.sum(block_energy * block_weight) / jnp.sum(block_weight), prop_data
+
+    @partial(jit, static_argnums=(0, 1, 3, 5))
+    def propagate_phaseless(
+        self,
+        ham: hamiltonian,
+        ham_data: dict,
+        prop: propagator,
+        prop_data: dict,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple[jax.Array, dict]:
+        def _sr_block_scan_wrapper(x, y):
+            return self._sr_block_scan(x, y, ham_data, prop, trial, wave_data)
+
+        prop_data["overlaps"] = trial.calc_overlap(prop_data["walkers"], wave_data)
+        prop_data["n_killed_walkers"] = 0
+        prop_data["pop_control_ene_shift"] = prop_data["e_estimate"]
+        prop_data, (block_energy, block_weight) = lax.scan(
+            _sr_block_scan_wrapper, prop_data, None, length=self.n_sr_blocks
+        )
+        prop_data["n_killed_walkers"] /= (
+            self.n_sr_blocks * self.n_ene_blocks * prop.n_walkers
+        )
+        return jnp.sum(block_energy * block_weight) / jnp.sum(block_weight), prop_data
+
+    @partial(jit, static_argnums=(0, 1, 3, 5))
+    def propagate_free(
+        self,
+        ham: hamiltonian,
+        ham_data: dict,
+        prop: propagator,
+        prop_data: dict,
+        trial: wave_function,
+        wave_data: dict,
+    ) -> Tuple:
+        def _block_scan_free_wrapper(x, y):
+            return self._block_scan_free(x, y, ham_data, prop, trial, wave_data)
+
+        prop_data["overlaps"] = trial.calc_overlap(prop_data["walkers"], wave_data)
+        prop_data, (prop_data_tr, block_energy, block_weight) = lax.scan(
+            _block_scan_free_wrapper, prop_data, None, length=self.n_blocks
+        )
+        return prop_data_tr, block_energy, block_weight, prop_data["key"]
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.__dict__.values()))
